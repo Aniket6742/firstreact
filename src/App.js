71b10a3fa3625d434bbcb1080e2374ad2bb20274
Aniket0620 <aniket6742@gmail.com>
2021-10-18 15:56:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>This is main Headding</h1>
+      <hr/>
+      <h1>This is app component</h1>
+      <hr/>
+      <h1>Footer Section</h1>
+    </div>
+  );
+}
+
+export default App;
